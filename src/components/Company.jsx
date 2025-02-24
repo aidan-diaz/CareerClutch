01722e7db195c5be834a9e0ba26f1408da2d612b
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-02-24 13:41:53 -0500

    added hover effect to items on hitlist

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -7,7 +7,7 @@ const Company = ({ id, name, title, location, onClick }) => {
     return (
         <li
             id={id}
-            className="flex flex-col justify-center items-center w-80 h-64 gap-y-4 border-2 border-card-cream rounded-lg shadow-xl bg-card-cream"
+            className="flex flex-col justify-center items-center w-80 h-64 gap-y-4 border-2 border-card-cream rounded-lg shadow-xl bg-card-cream hover:scale-105 transition duration-150 ease-in-out"
         >
             <CompanyName name={name} />
             <JobTitle title={title} />
