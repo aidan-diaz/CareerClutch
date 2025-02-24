45727dca8c3bfa827e31b3e5ba30a4dab873fccb
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-02-23 22:24:55 -0500

    created variables for useState hooks

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [companies, setCompanies] = useState([])
+  const [newCompanyName, setNewCompanyName] = useState('')
+  const [newJobDescription, setNewJobDescription] = useState('')
+  const [companyFilter, setCompanyFilter] = useState('')
 
   return (
     <>
@@ -11,3 +15,13 @@ function App() {
 }
 
 export default App
+
+
+//save companies to JSON server
+//allow ability to add companies
+//allow ability to delete companies
+//allow users to filter companies based on name
+//show an error if a user tries to add a company and role that already exist
+
+//PUSH WORK
+//allow users to edit company and role names
