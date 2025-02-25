728400a15ef7eaf25dd4268c23f7e39500a7db55
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-02-25 00:07:18 -0500

    updated button styles to have a pointer cursor on hover

--- src/components/CompanyForm.jsx
+++ src/components/CompanyForm.jsx
@@ -49,7 +49,7 @@ const CompanyForm = ({ onSubmit, handleCompanyNameChange, handleJobTitleNameChan
             </section>
             <div>
                 <button 
-                    className="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" type="submit">
+                    className="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none hover:cursor-pointer disabled:opacity-50 disabled:shadow-none ml-2" type="submit">
                         Add Company
                 </button>
             </div>
